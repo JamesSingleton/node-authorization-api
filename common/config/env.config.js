19e6c1282b5a402d33ab458e0f3c02d34861bfff
James Singleton <jamesrsingleton1@gmail.com>
2018-08-15 22:20:08 -0700

    Initial Commit

--- common/config/env.config.js
+++ common/config/env.config.js
@@ -2,7 +2,7 @@ module.exports = {
     "port": 3600,
     "appEndpoint": "http://localhost:3600",
     "apiEndpoint": "http;//localhost:3600",
-    "jwt_secret": "myS33!!creeeT",
+    "jwt_secret": "youS3CR3Tr1gHtHere!",
     "jwt_expiration_in_seconds": 36000,
     "environment": "dev",
     "permissionLevels": {
